9eacfed892b96e442996cf1dfb8ef97dec98f901
M.P <pakzad199@gmail.com>
2025-10-24 16:58:22 +0330

    adding base model and migrations

--- core/models.py
+++ core/models.py
@@ -1,14 +1,31 @@
 from django.db import models
-from core.abstract import BlameableModel
+from core.middleware.CurrentUserMiddleware import get_current_user
 from django_extensions.db.models import (
 	TimeStampedModel, 
 	ActivatorModel,
 )
-from django.conf import settings
+from django.contrib.auth.models import User
 
-User = settings.AUTH_USER_MODEL
+# User = settings.AUTH_USER_MODEL
 
-class Room(ActivatorModel, TimeStampedModel, BlameableModel, models.Model):
+class BaseModel(TimeStampedModel, ActivatorModel):
+    created_by = models.ForeignKey(User, related_name='created_%(class)s_set',
+                                   on_delete=models.SET_NULL, null=True, blank=True)
+    updated_by = models.ForeignKey(User, related_name='updated_%(class)s_set',
+                                   on_delete=models.SET_NULL, null=True, blank=True)
+    
+    class Meta:
+        abstract = True
+
+    def save(self, *args, **kwargs):
+        user = get_current_user()
+        if not self.pk and not self.created_by:
+            self.created_by = user
+        self.updated_by = user
+        super().save(*args, **kwargs)
+
+
+class Room(BaseModel):
     name = models.CharField(max_length=255)
     is_group = models.BooleanField(default=False)
     members = models.ManyToManyField(User, related_name='rooms')
@@ -17,7 +34,7 @@ def __str__(self):
     return self.name
 
 
-class Message(ActivatorModel, TimeStampedModel, BlameableModel, models.Model):
+class Message(BaseModel):
     room = models.ForeignKey(Room, related_name='messages', on_delete=models.CASCADE)
     sender = models.ForeignKey(User, related_name='sent_messages', on_delete=models.CASCADE)
     content = models.TextField(blank=True)
@@ -28,6 +45,6 @@ class Meta:
     ordering = ['-created_at']
 
 
-class Attachment(ActivatorModel, TimeStampedModel, BlameableModel, models.Model):
+class Attachment(BaseModel):
     message = models.ForeignKey(Message, related_name='attachments', on_delete=models.CASCADE)
     file = models.FileField(upload_to='attachments/%Y/%m/%d')
