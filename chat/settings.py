0a6c32e07639e7edefcadbc5314c26c1b557cca6
M.P <pakzad199@gmail.com>
2025-10-31 16:23:45 +0330

    add s3 package and configure it

--- chat/settings.py
+++ chat/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
     'room',
     'message',
     'drf_spectacular',
+    "storages",
 ]
 
 MIDDLEWARE = [
@@ -171,3 +172,20 @@ sentry_sdk.init(
     # of transactions for performance monitoring.
     traces_sample_rate=1.0,
 )
+
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
+
+AWS_S3_ENDPOINT_URL = env("AWS_S3_ENDPOINT_URL")
+AWS_ACCESS_KEY_ID = env("AWS_ACCESS_KEY_ID")
+AWS_SECRET_ACCESS_KEY = env("AWS_SECRET_ACCESS_KEY")
+AWS_STORAGE_BUCKET_NAME = env("AWS_STORAGE_BUCKET_NAME")
+AWS_S3_REGION_NAME = env("AWS_S3_REGION_NAME", None)
+AWS_DEFAULT_ACL = None
+AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
